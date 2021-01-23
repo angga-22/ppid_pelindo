e8c8cdcc281e598504a5d141b3ff3a6799e6ccf2
aztran <andra.riztyan@gmail.com>
2021-01-23 15:05:31 +0700

    setup new properties of tailwind, created banner component, replace banner maklumat & profile usng banner component, implement new font & line height in profile

--- src/containers/ProfilePage/index.tsx
+++ src/containers/ProfilePage/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { ProfilepageApi } from '../../api';
 import Loading from 'components/LoadingBar';
+import Banner from 'components/Banner';
 
 const Index = () => {
 	const [paragraf1, setParagraf1] = useState([]);
@@ -41,22 +42,26 @@ const Index = () => {
 				<Loading />
 			) : (
 				<div>
-					<div className='profile-banner w-full h-60'>
+					{/* <div className='profile-banner w-full h-60'>
 						<div className='block sm:hidden h-full d-flex items-center'>
 							<div className='text-5xl text-white h-full px-4 flex items-center'>Profile</div>
 						</div>
-					</div>
-					<div className='mt-16 container mx-auto'>
+					</div> */}
+					{/* <div className='flex items-center justify-between bg-blue-100 text-white p-6 lg:p-20'>
+						<h1 className='text-xl lg:text-6xl'>Maklumat</h1>
+					</div> */}
+					<Banner title='Profile' />
+					<div className='mt-16 container mx-auto layout-background'>
 						<div className='sm:w-8/12 mx-auto w-10/12'>
-							<img src={require('../../assets/img/logo-pelindo.png').default} alt='asset' className='sm:w-1/4 w-3/4' />
+							<img src={require('../../assets/img/logo-pelindo.png').default} alt='asset' className='sm:w-2/5 w-3/4 my-8' />
 
 							<div className='mt-8 mb-52'>
-								<p className='text-md sm:text-xl my-8 leading-loose'>{paragraf1}</p>
-								<p className='text-md sm:text-xl my-8 leading-loose'>{paragraf2}</p>
+								<p className='text-md sm:text-1em my-8 leading-loose'>{paragraf1}</p>
+								<p className='text-md sm:text-1em my-8 leading-loose'>{paragraf2}</p>
 
-								<div className='font-bold text-md sm:text-xl'>{nameCompany}</div>
+								<div className='font-bold text-md sm:text-1em'>{nameCompany}</div>
 
-								<div className='text-md sm:text-xl mt-8'>
+								<div className='text-md sm:text-1em mt-8'>
 									Jl. Perak Timur No. 610
 									<br />
 									Surabaya 60165 - Indonesia
